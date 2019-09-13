bbd16a0b93654cde1a0c40f882a9d03289611cee
ESTUDIANTE <estudiante@ul.edu.co>
2019-09-12 20:20:27 -0500

    otra vez

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GrupoaComponent } from './grupoa/grupoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrupoaComponent
   ],
   imports: [
     BrowserModule,
