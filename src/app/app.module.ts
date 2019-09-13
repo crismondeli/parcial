ecaf918b35130a04673d7e57ca028578ce5df30e
ESTUDIANTE <estudiante@ul.edu.co>
2019-09-12 20:52:07 -0500

    parcial realizado por luis navarro y cristian montiel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GrupoaComponent } from './grupoa/grupoa.component';
+import { GrupobComponent } from './grupob/grupob.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GrupoaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent,GrupoaComponent,GrupobComponent],
+  imports: [BrowserModule,AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
